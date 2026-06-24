c9624134b1b68f43ad9c04ff793fc6cdc61a4b47
Pawan Nishad <141106285+PAWAN0804@users.noreply.github.com>
2026-06-24 15:29:34 +0530

    how to become millionior game

---
+++ millionior.py
@@ -0,0 +1,35 @@
+questions = [
+    ["Who is Shah Rukh Khan?", "WWE Wrestler", "Plumber", "Actor", "Astronaut", 3],
+    ["What is the capital of France?", "Berlin", "Paris", "Rome", "London", 2],
+    ["Which planet is known as the Red Planet?", "Earth", "Venus", "Mars", "Jupiter", 3],
+    ["What is the largest mammal?", "Shark", "Blue Whale", "Elephant", "Giraffe", 2],
+    ["Who wrote 'Romeo and Juliet'?", "William Shakespeare", "Jane Austen", "Charles Dickens", "Homer", 1],
+    ["What is the square root of 64?", "8", "10", "6", "12", 1],
+    ["Which country is known as the Land of the Rising Sun?", "India", "South Korea", "Japan", "China", 3],
+    ["Who painted the Mona Lisa?", "Claude Monet", "Pablo Picasso", "Leonardo da Vinci", "Vincent van Gogh", 3],
+    ["What is the fastest land animal?", "Horse", "Lion", "Cheetah", "Elephant", 3],
+    ["Which ocean is the largest?", "Indian Ocean", "Pacific Ocean", "Atlantic Ocean", "Arctic Ocean", 2],
+    ["What is the smallest country in the world?", "San Marino", "Vatican City", "Monaco", "Liechtenstein", 2]
+]
+
+prize = [1000, 2000, 3000, 5000, 10000, 25000, 50000, 100000, 150000, 300000, 500000]
+
+i = 0
+for question in questions:
+    print(question[0])
+    print(f"a. {question[1]}")
+    print(f"b. {question[2]}")
+    print(f"c. {question[3]}")
+    print(f"d. {question[4]}")
+
+
+    # checking the answer
+    a = int(input("Enter your answer:\n1 for a, 2 for b, 3 for c, 4 for d\n"))
+    if (question[5] == a):
+        print("Correct answer")
+    else:
+        print(f"Incorrect!.Correct answer was {question[5]}")
+        print("Better luck next time.")
+        break
+    sum = print(f"you won {prize[i]}")
+    i +=1
